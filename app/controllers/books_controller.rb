792b7dd55159a043b6bc83af49d85a96bdb84c23
pentaku <ohara1728136@gmail.com>
2021-08-27 13:35:46 +0000

    [update]booksコントローラーのindex,show,edit,destory作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,12 @@
 class BooksController < ApplicationController
   def new
-    @book = Book.new
+    @bookform = Book.new
   end
 
   def create
-    @book = Book.new(book_params)
-    @book.save
-    redirect_to 'books/:id'
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
   end
 
   def index
@@ -15,6 +15,22 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
